7eeff27c19fb6e957170fda448071fd40e9d05c9
Blake Bartenbach <seed419@gmail.com>
2012-08-12 06:15:08 -0500

    Experimenting with Pilots radar jammer idea

--- org/hopto/seed419/listeners/CreeperPowerupListener.java
+++ org/hopto/seed419/listeners/CreeperPowerupListener.java
@@ -1,7 +1,6 @@
 package org.hopto.seed419.listeners;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,7 +13,8 @@ public class CreeperPowerupListener implements Listener {
 
 
     private CreeperNotify cn;
-    private final String alert = ChatColor.YELLOW + "[" + ChatColor.GREEN + "Creeper Alert" + ChatColor.YELLOW + "]";
+    private final String creeperAlert = ChatColor.YELLOW + "[" + ChatColor.GREEN + "Creeper Alert" + ChatColor.YELLOW + "]";
+    private final String creeperLost = ChatColor.YELLOW + "[" + ChatColor.RED + "Creeper Lost" + ChatColor.YELLOW + "]";
 
 
     public CreeperPowerupListener(CreeperNotify cn) {
@@ -24,34 +24,48 @@ public class CreeperPowerupListener implements Listener {
     @EventHandler
     public void onCreeperTarget(final EntityTargetLivingEntityEvent event) {
         if (event.getEntity().getType() == EntityType.CREEPER  && event.getTarget() instanceof Player) {
+
             final Player player = (Player) event.getTarget();
-            if (cn.getConfig().getBoolean(Config.useCreeperRadar) && player.getInventory().contains(Material.getMaterial(cn.getConfig().getInt(Config.radarToolID)))) {
-                if (player.hasPermission("CreeperNotify.notify") || !cn.getConfig().getBoolean(Config.usePerms)) {
-                    player.sendMessage(alert);
+
+            //Creeper radar only
+            if (cn.getConfig().getBoolean(Config.requireCreeperRadar) && player.getInventory().contains(Material.getMaterial(cn.getConfig().getInt(Config.radarToolID)))) {
+                if (cn.hasPerms(player)) {
+                    player.sendMessage(creeperAlert);
+
+                    /*Testing*/
+                    player.playEffect(EntityEffect.SHEEP_EAT);
+                    player.playEffect(event.getEntity().getLocation(), Effect.ZOMBIE_CHEW_IRON_DOOR, 1);
+
                 }
-                if (cn.getConfig().getInt(Config.radarToolID) == 345) {
-                    scheduleTask(event, player);
-                }
-            } else if (!cn.getConfig().getBoolean(Config.useCreeperRadar) && (player.hasPermission("CreeperNotify.notify") || !cn.getConfig().getBoolean(Config.usePerms))){
-                player.sendMessage(alert);
-                if (player.getInventory().contains(Material.getMaterial(cn.getConfig().getInt(Config.radarToolID)))) {
-                    player.setCompassTarget(event.getEntity().getLocation());
+                //else, do things the old way.
+            } else if (!cn.getConfig().getBoolean(Config.requireCreeperRadar)) {
+                if (cn.hasPerms(player)){
+                    player.sendMessage(creeperAlert);
+
+                    //Still update the compass if they wish, even if it's not required.
+                    if (cn.getConfig().getBoolean(Config.updateCompass) && player.getInventory().contains(Material.COMPASS)) {
+                        updateCompass(event, player);
+                    }
                 }
             }
         }
     }
 
-    public void scheduleTask(final EntityTargetLivingEntityEvent event, final Player player) {
+    //Compass location updating thread.  Sets compass to bed if entity dies or is out of range.
+    public void updateCompass(final EntityTargetLivingEntityEvent event, final Player player) {
         cn.getServer().getScheduler().scheduleSyncDelayedTask(cn, new Runnable() {
             @Override
             public void run() {
                 if (event.getTarget().equals(player) && !event.getEntity().isDead() && event.getTarget().getNearbyEntities(15.0, 15.0, 15.0).contains(event.getEntity())) {
                     player.setCompassTarget(event.getEntity().getLocation());
-                    scheduleTask(event, player);
+                    updateCompass(event, player);
                 } else {
                     player.setCompassTarget(player.getBedSpawnLocation());
+                    player.sendMessage(creeperLost);
                 }
             }
         });
     }
+
+
  }
---
+++ org/hopto/seed419/listeners/RadarJamListener.java
@@ -0,0 +1,126 @@
+package org.hopto.seed419.listeners;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.hopto.seed419.CreeperNotify;
+import org.hopto.seed419.file.Config;
+
+import java.util.HashSet;
+
+/**
+ * Attribute Only (Public) License
+ * Version 0.a3, July 11, 2011
+ * <p/>
+ * Copyright (C) 2012 Blake Bartenbach <seed419@gmail.com> (@seed419)
+ * <p/>
+ * Anyone is allowed to copy and distribute verbatim or modified
+ * copies of this license document and altering is allowed as long
+ * as you attribute the author(s) of this license document / files.
+ * <p/>
+ * ATTRIBUTE ONLY PUBLIC LICENSE
+ * TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ * <p/>
+ * 1. Attribute anyone attached to the license document.
+ * Do not remove pre-existing attributes.
+ * <p/>
+ * Plausible attribution methods:
+ * 1. Through comment blocks.
+ * 2. Referencing on a site, wiki, or about page.
+ * <p/>
+ * 2. Do whatever you want as long as you don't invalidate 1.
+ *
+ * @license AOL v.a3 <http://aol.nexua.org>
+ */
+public class RadarJamListener implements Listener {
+
+
+    private CreeperNotify cn;
+    private final String radarJammed = ChatColor.YELLOW + "[" + ChatColor.DARK_RED + "Radar Jammed" + ChatColor.YELLOW + "]";
+    private final String radarClear = ChatColor.YELLOW + "[" + ChatColor.GRAY + "Radar Online" + ChatColor.YELLOW + "]";
+    private final HashSet<String> jammedPlayers = new HashSet<String>();
+
+
+    public RadarJamListener(CreeperNotify cn) {
+        this.cn = cn;
+    }
+
+
+    @EventHandler
+    void onEndermanNearby(PlayerMoveEvent event) {
+        if (event.getPlayer().getInventory().contains(Material.COMPASS) && cn.getConfig().getBoolean(Config.radarJam)) {
+            boolean jammed = false;
+            for (Entity x : event.getPlayer().getNearbyEntities(15.0,15.0, 15.0)) {
+                if (x.getType() == EntityType.ENDERMAN && !jammedPlayers.contains(event.getPlayer().getName())) {
+                    System.out.println("enderman detected");
+                    jammedPlayers.add(event.getPlayer().getName());
+                    jammed = true;
+                    World world = event.getPlayer().getWorld();
+                    event.getPlayer().sendMessage(radarJammed);
+                    jamCompass(event, world, event.getPlayer().getLocation().getY());
+                    return;
+                }
+            }
+            if (!jammed && jammedPlayers.contains(event.getPlayer().getName())) {
+                boolean ender = false;
+                for (Entity x : event.getPlayer().getNearbyEntities(15.0,15.0,15.0)) {
+                    if (x.getType() == EntityType.ENDERMAN) {
+                        ender = true;
+                        return;
+                    }
+                }
+                if (!ender) {
+                    System.out.println("compass clear");
+                    event.getPlayer().setCompassTarget(event.getPlayer().getBedSpawnLocation());
+                    event.getPlayer().sendMessage(radarClear);
+                    jammedPlayers.remove(event.getPlayer().getName());
+                }
+            }
+        }
+
+    }
+
+    public void jamCompass(final PlayerMoveEvent event, final World world,  final double y) {
+        cn.getServer().getScheduler().scheduleAsyncDelayedTask(cn, new Runnable() {
+            @Override
+            public void run() {
+                for (Entity x : event.getPlayer().getNearbyEntities(15.0, 15.0, 15.0)) {
+                    if (x.getType() == EntityType.ENDERMAN) {
+                        //System.out.println("jamming compass");
+                        event.getPlayer().setCompassTarget(new Location(world, event.getPlayer().getLocation().getX(), y, (event.getPlayer().getLocation().getZ() + 5.0)));
+                        sleep();
+                        event.getPlayer().setCompassTarget(new Location(world, (event.getPlayer().getLocation().getX() - 5.0), y, (event.getPlayer().getLocation().getZ() + 5.0)));
+                        sleep();
+                        event.getPlayer().setCompassTarget(new Location(world, (event.getPlayer().getLocation().getX() - 5.0), y, event.getPlayer().getLocation().getX()));
+                        sleep();
+                        event.getPlayer().setCompassTarget(new Location(world, (event.getPlayer().getLocation().getX() - 5.0), y, (event.getPlayer().getLocation().getZ() - 5.0)));
+                        sleep();
+                        event.getPlayer().setCompassTarget(new Location(world, event.getPlayer().getLocation().getX(), y, (event.getPlayer().getLocation().getZ() - 5.0)));
+                        sleep();
+                        event.getPlayer().setCompassTarget(new Location(world, (event.getPlayer().getLocation().getX() + 5.0), y, (event.getPlayer().getLocation().getZ() - 5.0)));
+                        sleep();
+                        event.getPlayer().setCompassTarget(new Location(world, (event.getPlayer().getLocation().getX() + 5.0), y, event.getPlayer().getLocation().getZ()));
+                        sleep();
+                        event.getPlayer().setCompassTarget(new Location(world, (event.getPlayer().getLocation().getX() + 5.0), y, (event.getPlayer().getLocation().getZ() + 5.0)));
+                        sleep();
+                        jamCompass(event, world, y);
+                    }
+                }
+                event.getPlayer().setCompassTarget(event.getPlayer().getBedSpawnLocation());
+
+            }
+        });
+    }
+
+    void sleep() {
+        try {
+            Thread.sleep(300);
+        } catch (InterruptedException e) {}
+    }
+}
